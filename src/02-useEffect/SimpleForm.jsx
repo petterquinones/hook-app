5af0fe48cb01a2b926d14535b9219c867174122d
Petter0915 <petter0915@gmail.com>
2024-07-02 23:04:18 +0200

    Update useEffect hooks

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,6 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
 
 
 export const SimpleForm = () => {
+
+    const [FormState, setfFormState] = useState({
+        username:'goku',
+        email:'goku@gmail.com'
+    })
+
+    const {username, email} = FormState;
+
+    const onInputChange = ({target})=>{
+        const {name, value} = target;
+        setfFormState({
+            ...FormState,
+            [name]:value
+        })
+    }
+
+    useEffect(()=>{
+        console.log('useEffect called')
+    })
+
   return (
     <>
         <h1> Simple form </h1>
@@ -10,6 +32,8 @@ export const SimpleForm = () => {
             className="form-control"
             placeholder="Username"
             name="username"
+            value={username}
+            onChange={onInputChange}
         />
 
         
@@ -18,6 +42,8 @@ export const SimpleForm = () => {
             className="form-control mt-2"
             placeholder="petter0915@gmail.com"
             name="email"
+            value={email}
+            onChange={onInputChange}
         />
     </>
   )
